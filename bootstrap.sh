d223796c803ca4979229f14857b8f73eef59ebf6
o2akky <44025274+o2akky@users.noreply.github.com>
2022-02-03 18:11:26 +0900

    Create bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+# 基本セットアップ
+#
+#
+if [ ! -d ~/.ssh ]; then
+  mkdir -m 700 ~/.ssh
+fi
+touch ~/.ssh/authorized_keys
+chmod 600 ~/.ssh/authorized_keys
+
+curl -s https://github/o2akky.keys >> ~/.ssh/authorized_keys
+curl -s https://github/picturecode.keys >> ~/.ssh/authorized_keys
